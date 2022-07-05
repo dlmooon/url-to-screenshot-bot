b91e9c4e8e2d3447a44af9ef36702b2064d4151d
DlMOOON <vip.dima2005@gmail.com>
2022-07-05 16:23:34 +0500

    add path to chromedriver and chrome in heroku

--- src/main/java/com/bot/screenshoter/TelegramBotApplication.java
+++ src/main/java/com/bot/screenshoter/TelegramBotApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TelegramBotApplication {
     public static void main(String[] args) {
+        System.setProperty("GOOGLE_CHROME_BIN", "/app/.apt/usr/bin/google-chrome");
+        System.setProperty("CHROMEDRIVER_PATH", "/app/.chromedriver/bin/chromedriver");
         SpringApplication.run(TelegramBotApplication.class, args);
     }
 }
