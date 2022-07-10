f0f40c7ed89d03c31195094f5baf1a82c32985ac
DlMOOON <vip.dima2005@gmail.com>
2022-07-11 00:34:29 +0500

    Enable scheduling

--- src/main/java/com/bot/screenshoter/TelegramBotApplication.java
+++ src/main/java/com/bot/screenshoter/TelegramBotApplication.java
@@ -3,8 +3,10 @@ package com.bot.screenshoter;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TelegramBotApplication {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
