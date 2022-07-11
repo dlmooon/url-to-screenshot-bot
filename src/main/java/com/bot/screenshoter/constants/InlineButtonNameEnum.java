1fda33e65764b16bb02f0687ac709cafe190839a
DlMOOON <vip.dima2005@gmail.com>
2022-07-11 23:48:00 +0500

    Feat: new buttons for new inline keyboard

--- src/main/java/com/bot/screenshoter/constants/InlineButtonNameEnum.java
+++ src/main/java/com/bot/screenshoter/constants/InlineButtonNameEnum.java
@@ -4,7 +4,10 @@ public enum InlineButtonNameEnum {
 
     SIMPLE_SCREENSHOT_BUTTON(EmojiEnum.MONITOR.get() + " Простой скриншот"),
     LONG_SCREENSHOT_BUTTON  (EmojiEnum.TAPE.get()    + " Длинный скриншот"),
-    CUSTOM_SCREENSHOT_BUTTON(EmojiEnum.GEAR .get()   + " Кастомный скриншот");
+    CUSTOM_SCREENSHOT_BUTTON(EmojiEnum.GEAR .get()   + " Кастомный скриншот"),
+
+    CONFIRM_BUTTON(EmojiEnum.CONFIRM.get() + " Подтвердить"),
+    CANCEL_BUTTON (EmojiEnum.CANCEL.get()  + " Отменить");
 
     private final String buttonText;
 
