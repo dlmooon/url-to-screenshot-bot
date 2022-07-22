9b8e8ac176fa601da86d0056427b36f76a7e6e9c
DlMOOON <vip.dima2005@gmail.com>
2022-07-22 16:25:17 +0500

    Refactor

--- src/main/java/com/bot/screenshoter/telegram/WebhookController.java
+++ src/main/java/com/bot/screenshoter/telegram/WebhookController.java
@@ -1,5 +1,6 @@
 package com.bot.screenshoter.telegram;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,14 +12,11 @@ import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @RestController
+@AllArgsConstructor
 public class WebhookController {
 
-    private final UrlToScreenshotBot bot;
-
     @Autowired
-    public WebhookController(UrlToScreenshotBot bot) {
-        this.bot = bot;
-    }
+    private final UrlToScreenshotBot bot;
 
     @PostMapping("/")
     public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
