dc99950f9358632c00f313a0a8dd3f41e539991c
DlMOOON <vip.dima2005@gmail.com>
2022-07-24 19:28:49 +0500

    Feat: registration user

--- src/main/java/com/bot/screenshoter/telegram/facade/handlers/CommandHandler.java
+++ src/main/java/com/bot/screenshoter/telegram/facade/handlers/CommandHandler.java
@@ -1,6 +1,7 @@
 package com.bot.screenshoter.telegram.facade.handlers;
 
 import com.bot.screenshoter.keyboards.ReplyKeyboardMaker;
+import com.bot.screenshoter.repository.UsersRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
@@ -13,12 +14,16 @@ public class CommandHandler {
     @Autowired
     ReplyKeyboardMaker replyKeyboardMaker;
 
+    @Autowired
+    UsersRepo usersRepo;
+
     public BotApiMethod<?> processCommand(Message message) {
         String chatId = message.getChatId().toString();
         String inputText = message.getText();
 
         switch (inputText) {
             case "/start":
+                usersRepo.register(message.getFrom());
                 SendMessage sendMessage = new SendMessage(chatId, "Привет");
                 sendMessage.setReplyMarkup(replyKeyboardMaker.getMainKeyboard());
                 sendMessage.enableMarkdown(true);
@@ -31,7 +36,7 @@ public class CommandHandler {
 
             default:
                 return new SendMessage(chatId, "Неизвестная команда: " + message.getText() + ".\n" +
-                                                    "Используйте /help для получения актуальных команд.");
+                        "Используйте /help для получения актуальных команд.");
         }
     }
 }
