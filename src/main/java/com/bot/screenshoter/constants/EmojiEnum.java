05918396ab965086e4b0ab5a481846a83f9c7703
DlMOOON <vip.dima2005@gmail.com>
2022-07-27 22:36:47 +0500

    Add new emoji

--- src/main/java/com/bot/screenshoter/constants/EmojiEnum.java
+++ src/main/java/com/bot/screenshoter/constants/EmojiEnum.java
@@ -2,18 +2,19 @@ package com.bot.screenshoter.constants;
 
 public enum EmojiEnum {
 
-    RUSSIA  ("\uD83C\uDDF7\uD83C\uDDFA"),
-    ENGLAND ("\uD83C\uDDEC\uD83C\uDDE7"),
-    CAMERA  ("\uD83D\uDCF7"),
-    PERSON  ("\uD83D\uDC64"),
-    ROCKET  ("\uD83D\uDE80"),
-    MONITOR ("\uD83D\uDDA5"),
-    TAPE    ("\uD83C\uDF9E"),
-    CROWN   ("\uD83D\uDC51"),
-    CAP     ("\uD83E\uDDE2"),
-    CANCEL  ("\u274C"),
-    CONFIRM ("\u2705"),
-    GEAR    ("\u2699");
+    RUSSIA    ("\uD83C\uDDF7\uD83C\uDDFA"),
+    ENGLAND   ("\uD83C\uDDEC\uD83C\uDDE7"),
+    CAMERA    ("\uD83D\uDCF7"),
+    PERSON    ("\uD83D\uDC64"),
+    ROCKET    ("\uD83D\uDE80"),
+    MONITOR   ("\uD83D\uDDA5"),
+    TAPE      ("\uD83C\uDF9E"),
+    CROWN     ("\uD83D\uDC51"),
+    CAP       ("\uD83E\uDDE2"),
+    LIGHTNING ("\u26A1"),
+    CANCEL    ("\u274C"),
+    CONFIRM   ("\u2705"),
+    GEAR      ("\u2699");
 
     private final String emoji;
 
