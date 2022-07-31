405cd0a373f8d85d9895288c12ad00b6f8026e39
DlMOOON <vip.dima2005@gmail.com>
2022-08-01 00:34:04 +0500

    Add new method for send bot action

--- src/main/java/com/bot/screenshoter/telegram/Bot.java
+++ src/main/java/com/bot/screenshoter/telegram/Bot.java
@@ -4,6 +4,7 @@ import com.bot.screenshoter.telegram.facade.TelegramFacade;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendChatAction;
 import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
@@ -57,13 +58,17 @@ public class Bot extends SpringWebhookBot {
         }
     }
 
+    public void sendBotAction(SendChatAction chatAction) {
+        try {
+            execute(chatAction);
+        } catch (TelegramApiException e) {
+            log.warn("Failed to send chat action", e);
+        }
+    }
+
     @Override
     public String getBotUsername() {
         return botUsername;
-    }
-
-    public void setBotUsername(String botUsername) {
-        this.botUsername = botUsername;
     }
 
     @Override
@@ -71,13 +76,17 @@ public class Bot extends SpringWebhookBot {
         return botToken;
     }
 
-    public void setBotToken(String botToken) {
-        this.botToken = botToken;
-    }
-
     @Override
     public String getBotPath() {
         return botWebhookPath;
+    }
+
+    public void setBotUsername(String botUsername) {
+        this.botUsername = botUsername;
+    }
+
+    public void setBotToken(String botToken) {
+        this.botToken = botToken;
     }
 
     public void setBotPath(String botWebhookPath) {
