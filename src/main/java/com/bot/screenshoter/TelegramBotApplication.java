9ec3daae5c62d8fe9bbda2c2ccd7f7959dddda41
DlMOOON <vip.dima2005@gmail.com>
2022-08-11 14:55:32 +0500

    Delete annotation for enable scheduling

--- src/main/java/com/bot/screenshoter/TelegramBotApplication.java
+++ src/main/java/com/bot/screenshoter/TelegramBotApplication.java
@@ -2,10 +2,8 @@ package com.bot.screenshoter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class TelegramBotApplication {
     public static void main(String[] args) {
         SpringApplication.run(TelegramBotApplication.class, args);
