4f80bb18e4b7db98bf1ece50a20a028f5dd4c94c
DlMOOON <vip.dima2005@gmail.com>
2022-08-18 14:34:35 +0500

    Add methods for send and delete message, delete method for send all users info

--- src/main/java/com/bot/screenshoter/MessageSender.java
+++ src/main/java/com/bot/screenshoter/MessageSender.java
@@ -1,29 +1,51 @@
 package com.bot.screenshoter;
 
-import com.bot.screenshoter.repository.UsersRepo;
 import com.bot.screenshoter.telegram.Bot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.User;
-
-import java.util.List;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 
 @Service
 public class MessageSender {
 
-    @Autowired
-    UsersRepo usersRepo;
-
     @Lazy
     @Autowired
-    Bot bot;
+    private Bot bot;
 
-    public void sendAllUsersAboutUpdate() {
-        List<User> users = usersRepo.getAllUsers();
-        for (User user : users) {
-            bot.sendMessage(new SendMessage(user.getId().toString(), "An update is out! Please restart the bot (call the /start method)\n\nThe bot is under active development, please share your feedback -> @listener69"));
-        }
+    @Autowired
+    private LocaleMessageService localeService;
+
+    public void send(String chatId, String text) {
+        String localeText = localeService.getById(chatId, text);
+        SendMessage message = new SendMessage(chatId, localeText);
+        bot.sendMessage(message);
+    }
+
+    public void send(String chatId, String text, ReplyKeyboard replyKeyboard) {
+        String localeText = localeService.getById(chatId, text);
+        SendMessage message = new SendMessage(chatId, localeText);
+        message.setReplyMarkup(replyKeyboard);
+        bot.sendMessage(message);
+    }
+
+    public void send(String chatId, String text, String... args) {
+        String localeText = localeService.getById(chatId, args, text);
+        SendMessage message = new SendMessage(chatId, localeText);
+        bot.sendMessage(message);
+    }
+
+    public void send(String chatId, String text, ReplyKeyboard replyKeyboard, String... args) {
+        String localeText = localeService.getById(chatId, args, text);
+        SendMessage message = new SendMessage(chatId, localeText);
+        message.setReplyMarkup(replyKeyboard);
+        bot.sendMessage(message);
+    }
+
+    public void delete(String chatId, Integer messageId) {
+        DeleteMessage message = new DeleteMessage(chatId, messageId);
+        bot.deleteMessage(message);
     }
 }
