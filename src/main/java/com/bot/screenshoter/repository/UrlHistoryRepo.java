b47a7b25aa65fd3814e1657c98b1407460303c23
DlMOOON <vip.dima2005@gmail.com>
2022-08-29 16:01:48 +0500

    Method addUrl now returns autogenerated url_id, method setIsSent use url_id for setting value is_sent column

--- src/main/java/com/bot/screenshoter/repository/UrlHistoryRepo.java
+++ src/main/java/com/bot/screenshoter/repository/UrlHistoryRepo.java
@@ -2,7 +2,11 @@ package com.bot.screenshoter.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
 
 @Repository
 public class UrlHistoryRepo {
@@ -10,16 +14,24 @@ public class UrlHistoryRepo {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-    public void addUrl(String url, String type, Long chatId) {
-        jdbcTemplate.update("INSERT INTO url_history " +
+    public Long addUrl(String url, String type, Long chatId) {
+        String sql = "INSERT INTO url_history " +
                 "(url, type, is_sent, timestamp_request, user_id) " +
-                "VALUES (?, ?, false, current_timestamp(3), ?)",
-                url,
-                type,
-                chatId);
+                "VALUES (?, ?, false, current_timestamp(3), ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(sql, new String[]{"url_id"});
+            ps.setString(1, url);
+            ps.setString(2, type);
+            ps.setLong(3, chatId);
+            return ps;
+        }, keyHolder);
+
+        return keyHolder.getKey().longValue();
     }
 
-    public void setIsSent(Long chatId, boolean isSent) {
-        jdbcTemplate.update("UPDATE url_history SET is_sent = ? WHERE user_id = ?", isSent, chatId);
+    public void setIsSent(Long url_id, boolean isSent) {
+        jdbcTemplate.update("UPDATE url_history SET is_sent = ? WHERE url_id = ?", isSent, url_id);
     }
 }
