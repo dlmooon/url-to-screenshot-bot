031b2403f3783f5d15e10203ad6db6db30b62de9
DlMOOON <vip.dima2005@gmail.com>
2022-08-29 15:55:06 +0500

    Minor refactor

--- src/main/java/com/bot/screenshoter/bot/handlers/impl/UpdateHandler.java
+++ src/main/java/com/bot/screenshoter/bot/handlers/impl/UpdateHandler.java
@@ -1,6 +1,6 @@
-package com.bot.screenshoter.handlers.impl;
+package com.bot.screenshoter.bot.handlers.impl;
 
-import com.bot.screenshoter.handlers.Handler;
+import com.bot.screenshoter.bot.handlers.Handler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
