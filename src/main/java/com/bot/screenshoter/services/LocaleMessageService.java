93702450fce2108e074b5f2d88c675067b0448c5
DlMOOON <vip.dima2005@gmail.com>
2022-08-29 15:27:54 +0500

    Minor refactor

--- src/main/java/com/bot/screenshoter/services/LocaleMessageService.java
+++ src/main/java/com/bot/screenshoter/services/LocaleMessageService.java
@@ -1,4 +1,4 @@
-package com.bot.screenshoter;
+package com.bot.screenshoter.services;
 
 import com.bot.screenshoter.repository.UsersBotLangRepo;
 import org.springframework.beans.factory.annotation.Autowired;
