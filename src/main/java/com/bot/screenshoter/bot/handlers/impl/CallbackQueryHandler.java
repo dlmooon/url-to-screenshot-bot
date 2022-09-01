f706efed27a383dbba3a38072f3512abc60c0357
DlMOOON <vip.dima2005@gmail.com>
2022-09-01 17:12:53 +0500

    Refactor method for handle update

--- src/main/java/com/bot/screenshoter/bot/handlers/impl/CallbackQueryHandler.java
+++ src/main/java/com/bot/screenshoter/bot/handlers/impl/CallbackQueryHandler.java
@@ -3,25 +3,21 @@ package com.bot.screenshoter.bot.handlers.impl;
 import com.bot.screenshoter.bot.handlers.Handler;
 import com.bot.screenshoter.constants.InlineButtonEnum;
 import com.bot.screenshoter.services.TelegramService;
-import com.bot.screenshoter.services.actions.impl.SelectConfirmOrCancelAction;
-import com.bot.screenshoter.services.actions.impl.SelectLangAction;
-import com.bot.screenshoter.services.actions.impl.SelectScreenshotTypeAction;
+import com.bot.screenshoter.services.actions.InlineKeyboardAction;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+import java.util.List;
+
 @Slf4j
 @Service
 public class CallbackQueryHandler implements Handler {
 
     @Autowired
-    SelectScreenshotTypeAction selectScreenshotTypeAction;
-    @Autowired
-    SelectLangAction selectLangAction;
-    @Autowired
-    SelectConfirmOrCancelAction selectConfirmOrCancelAction;
+    private List<InlineKeyboardAction> inlineActions;
     @Autowired
     private TelegramService telegramService;
 
@@ -41,26 +37,10 @@ public class CallbackQueryHandler implements Handler {
         String chatId = callbackQuery.getMessage().getChatId().toString();
         InlineButtonEnum button = InlineButtonEnum.valueOf(callbackQuery.getData());
         telegramService.deleteMessage(chatId, callbackQuery.getMessage().getMessageId());
-        switch (button) {
-            case SIMPLE_SCREENSHOT_BUTTON:
-            case LONG_SCREENSHOT_BUTTON:
-            case CUSTOM_SCREENSHOT_BUTTON:
-            case CONFIRM_CUSTOM_SCREENSHOT_BUTTON:
-                selectScreenshotTypeAction.handle(chatId, button);
-                break;
 
-            case RUSSIA_LANG_BUTTON:
-            case ENGLISH_LANG_BUTTON:
-                selectLangAction.handle(chatId, button);
-                break;
-
-            case CANCEL_BUTTON:
-                selectConfirmOrCancelAction.handle(chatId, button);
-                break;
-
-            default:
-                log.warn("Failed to handle button: {}", button);
-                telegramService.sendMessage(chatId, "something_wrong");
-        }
+        inlineActions.stream()
+                .filter(inlineKeyboardAction -> inlineKeyboardAction.supports(button))
+                .findFirst()
+                .ifPresent(inlineKeyboardAction -> inlineKeyboardAction.handle(chatId, button));
     }
 }
